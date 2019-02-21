9ad070570922c7af50316fbed028f95c225f6ccf
Steve Albonico <salbonico@gmail.com>
2019-02-21 18:49:59 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190221184405) do
+ActiveRecord::Schema.define(version: 20190221184946) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,10 +22,9 @@ ActiveRecord::Schema.define(version: 20190221184405) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "published_status"
   end
 
 end
